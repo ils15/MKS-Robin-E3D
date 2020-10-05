5165cae98d73d5c3960a0a4bc4333dce42d0f434
“ils15” <igorleite-ils@hotmail.com>
2020-10-05 07:43:40 -0300

    update board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -127,7 +127,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_ROBIN_E3
+  #define MOTHERBOARD BOARD_MKS_ROBIN_E3D
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
